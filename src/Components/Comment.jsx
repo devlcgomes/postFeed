3bbdb3de1dbf136a41ca247386cba3c3b859bdb3
Luciano Gomes <dev.lucianogomes@gmail.com>
2023-08-21 13:18:06 -0300

    Creation of the likes function in each post

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,8 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  // State for Like Count
+  const [likeCount, setLikeCount] = useState(0);
+
+  // handleLikeComment
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
   // Delete comment
   function handleDeleteComment() {
     alert("deletado");
@@ -27,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
